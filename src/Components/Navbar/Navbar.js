d2f6977fce48cb2b746e8781be591933bd9db609
Katy St Sauveur <katyrogowski@yahoo.com>
2021-06-11 15:27:48 -0700

    Built basic bones for Navbar component

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    const { player1, player, ties, userName1, userName2 } = this.props;
+    return (
+      <div className='navbar'>
+        <div className='title'>
+          <h1>Tic Tac Toe</h1>
+        </div>
+        <div className='statistics'>
+          <h2>
+            {userName1}:{player1}
+          </h2>
+          <h2>
+            {userName2}:{player2}
+          </h2>
+          <h2>Ties: {ties}</h2>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
