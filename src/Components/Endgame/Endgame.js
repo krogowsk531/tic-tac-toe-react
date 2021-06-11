8d739d5c3f08c31e8ddeb71d5744187bf961d114
Katy St Sauveur <katyrogowski@yahoo.com>
2021-06-11 15:18:18 -0700

    Built basic parts of Endgame component

---
+++ src/Components/Endgame/Endgame.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Component } from 'react'
+
+class Endgame extends Component {
+  state = {
+    tied: 'You are tied',
+    playerWin: 'You win ' + this.props.winner + ' !!',
+  }
+  
+  handleClick = () => {
+    this.props.endgame(false);
+  }
+  
+  render() {
+    const { winner } = this.props;
+    const { tied, playerWin } = this.state;
+    return (
+      <div className='wrapper'>
+        <div className='screen'>
+          <p> {winner === 'Tied' ? tied : playerWin} </p>
+          <button className='btn btn-primary' onClick={this.handleClick}>
+            Try Again
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Endgame; 
